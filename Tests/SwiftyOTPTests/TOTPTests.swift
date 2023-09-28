2285e2913e836d310de81021936798a5f56fcb6a
Lorenzo Limoli <l.limoli@reply.it>
2023-09-28 11:47:15 +0200

    Improve readability on tests

--- Tests/SwiftyOTPTests/TOTPTests.swift
+++ Tests/SwiftyOTPTests/TOTPTests.swift
@@ -4,9 +4,9 @@ import XCTest
 
 final class TOTPTests: XCTestCase {
 
-    let dataSHA1 = "12345678901234567890".data(using: String.Encoding.ascii)!
-    let dataSHA256 = "12345678901234567890123456789012".data(using: String.Encoding.ascii)!
-    let dataSHA512 = "1234567890123456789012345678901234567890123456789012345678901234".data(using: String.Encoding.ascii)!
+    let dataSHA1 = "12345678901234567890".data(using: .ascii)!
+    let dataSHA256 = "12345678901234567890123456789012".data(using: .ascii)!
+    let dataSHA512 = "1234567890123456789012345678901234567890123456789012345678901234".data(using: .ascii)!
     
     
     func test_otp_throwsDigitsOutBoundError() throws {
