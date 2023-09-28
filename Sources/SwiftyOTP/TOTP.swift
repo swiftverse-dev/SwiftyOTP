99a9f6f92e80da11b3bfa1a4c1ae7d43f69aa39d
Lorenzo Limoli <l.limoli@reply.it>
2023-09-28 11:14:18 +0200

    Added safe initializer with 6 digits as default

--- Sources/SwiftyOTP/TOTP.swift
+++ Sources/SwiftyOTP/TOTP.swift
@@ -23,11 +23,34 @@ public struct TOTP {
             let digits = switch self{
             case .digitsNumberOutOfBounds(let n): n
             }
-            return "Expected digits number in [6,8] interval. Got \(digits)"
+            return "Expected digits number in (6...8) interval. Got \(digits)"
         }
     }
     
-    public init(seed: Data, digits: Int = 6, timeStep: UInt64 = 30, algorithm: HashingAlgorithm = .sha1) throws {
+    /// Initializes a Time-Based OTP generator of 6 digits with the provided seed, time step, and hashing algorithm.
+    ///
+    /// - Parameters:
+    ///   - seed: The secret seed data used for generating OTPs.
+    ///   - timeStep: The time step duration in seconds. The default is 30 seconds.
+    ///   - algorithm: The hashing algorithm to use for OTP generation. The default is SHA-1.
+    public init(seed: Data, timeStep: UInt64 = 30, algorithm: HashingAlgorithm = .sha1) {
+        self.seed = seed
+        self.digits = 6
+        self.timeStep = timeStep
+        self.algorithm = algorithm
+    }
+
+    
+    /// Initializes a Time-Based OTP generator with the provided seed, number of digits, time step, and hashing algorithm.
+    ///
+    /// - Parameters:
+    ///   - seed: The secret seed data used for generating OTPs.
+    ///   - digits: The number of digits in the generated OTP. Must be within the range (6...8).
+    ///   - timeStep: The time step duration in seconds. The default is 30 seconds.
+    ///   - algorithm: The hashing algorithm to use for OTP generation. The default is SHA-1.
+    /// - Throws:
+    ///   - `Error.digitsNumberOutOfBounds`: If the `digits` parameter is not within the valid range.
+    public init(seed: Data, digits: Int, timeStep: UInt64 = 30, algorithm: HashingAlgorithm = .sha1) throws {
         guard Self.isDigitsNumberValid(digits) else { throw Error.digitsNumberOutOfBounds(digits) }
         self.seed = seed
         self.digits = Int(digits)
@@ -35,10 +58,13 @@ public struct TOTP {
         self.algorithm = algorithm
     }
     
+    
+    /// The current One-Time Password (OTP) for the current time.
     public var currentOTP: String {
         otp(at: Self.defaultDate())
     }
-    
+
+    /// Generate the One-Time Password (OTP) for the provided Date.
     public func otp(at date: Date) -> String {
         let stepCounter = stepCounter(at: date)
         let counterMessage = stepCounter.bigEndian.data
@@ -76,23 +102,23 @@ public extension TOTP {
         }
     }
     
+    /// The One-Time Password (OTP) for the provided Unix Timestamp.
     func otp(unixTimestamp timestamp: UnixTimestamp) -> String {
         otp(at: Date(timeIntervalSince1970: timestamp.timestampInSeconds))
     }
 }
 
-// Helpers
+// MARK: Helpers
 private extension TOTP {
-    
-    static func isDigitsNumberValid(_ digits: Int) -> Bool {
-        (6...8) ~= digits
-    }
+    static func isDigitsNumberValid(_ digits: Int) -> Bool { (6...8) ~= digits }
     
     func stepCounter(at date: Date) -> UInt64 {
         (date.timeIntervalSince1970.floor / timeStep.asDouble).floor.asUInt
     }
 }
 
+
+// MARK: HashingAlgorithm - Hmac
 private extension HashingAlgorithm {
     func hmac(for data: Data, using secret: Data) -> Data{
         switch self {
